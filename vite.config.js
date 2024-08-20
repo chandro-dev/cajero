99447d21dcf76e3e7ae87ce5151b0ab36cd321d3
Aejandro <alejocarreteroballesteros@gmail.com>
2024-08-20 07:49:46 -0500

    configbaseurl

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/alejocarreteroweb/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+
+    },
+      outDir:'dist'
+  },
 })
