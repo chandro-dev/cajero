e944091d4b89d9b6e12a8a5a4e5e8b07c97016a8
Aejandro <alejocarreteroballesteros@gmail.com>
2024-08-19 22:30:42 -0500

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+function App() {
+  const [monto, setMonto] = useState(0);
+  const denominaciones = [10, 20, 50, 100];
+  let auxiliar = 0;
+  let resultado = [];
+  let acarreo = 0;
+  let i = 0;
+  const calcularDenominaciones = () => {
+    resultado.push([]);
+    while (i < denominaciones.length) {
+      if ((auxiliar + denominaciones[i]) > monto) {
+        for (let j = i; j >= denominaciones.length; j--) {
+          if (auxiliar + denominaciones[j] >= monto) {
+            resultado.push([]);
+            auxiliar += denominaciones[j];
+            resultado[resultado.length - 1].push(denominaciones[j]);
+          }
+        }
+        i++;
+      } else {
+        auxiliar += denominaciones[i];
+        resultado[resultado.length - 1].push(denominaciones[i]);
+        i++;
+      }
+      if ((i == 4) && (auxiliar != monto)) {
+        resultado.push([]);
+        acarreo++;
+        for(var g=0;g<acarreo;g++){
+          resultado[resultado.length-1][g]=0; 
+        }
+        i = acarreo;
+      }
+    }
+    console.log(resultado);
+  }
+  // Maneja el cambio en el campo de entrada
+  const manejarCambio = (evento) => {
+    // Extrae el valor del campo de entrada y conviértelo a número
+    const nuevoMonto = parseFloat(evento.target.value);
+
+    // Actualiza el estado con el nuevo monto
+    setMonto(nuevoMonto);
+  };
+
+  calcularDenominaciones();
+  return (
+    <>
+      <div className=" text-5xl	flex justify-center items-center min-h-screen">
+        <div className="bg-white p-8 rounded-lg shadow-lg text-center">
+          <h2><strong>Monto</strong>:<input
+            type="number"
+            value={monto}
+            onChange={manejarCambio}
+          /></h2>
+          <table className="table-fixed">
+            <thead>
+              <tr>
+                {denominaciones.map((denominacion, index) => (
+                  <th className="p-2" key={index}>{denominacion}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+
+              {resultado.map((fila, filaIndex) => (
+                <tr key={filaIndex} className={fila}>
+                  {fila.map((denominacion, index) => (
+                    <td className="p-2" key={index}>{denominacion}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+
+          </table>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
